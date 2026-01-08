d98ce75fea0ed69b2eb882598c2b19186bf20fa7
Akshay Prajapati <13akshay1995@gmail.com>
2026-01-07 18:17:12 -0800

    Refactor packages and correct Spring Boot starter setup

--- src/main/java/com/akshay/notionlite/realtime/RealtimeServiceApplication.java
+++ src/main/java/com/akshay/notionlite/realtime/RealtimeServiceApplication.java
@@ -1,4 +1,4 @@
-package com.akshay.notionlite;
+package com.akshay.notionlite.realtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/akshay/notionlite/realtime/RealtimeServiceApplicationTests.java
+++ src/test/java/com/akshay/notionlite/realtime/RealtimeServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.akshay.notionlite;
+package com.akshay.notionlite.realtime;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
